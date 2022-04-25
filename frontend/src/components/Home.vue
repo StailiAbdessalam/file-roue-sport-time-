ef7dd5830ee37fdf4391056b91d3090ade11e6a7
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-25 16:17:42 +0000

    .

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="flex flex-col justify-center items-center gap-20">
+        <div class="flex flex-col items-center justify-center w-full mt-20 gap-5">
+            <h1 class="font-bold md:text-5xl sm:text-xl text-center">A small business is only as good as <br> its tools.
+            </h1>
+            <p class="text-center sm:text-lg ">We’re different. Flex is the only saas business platform that lets you
+                run your business on one platform, seamlessly across all digital channels.</p>
+            <div class="w-1/2 relative">
+                <img class="w-full h-full" src="../assets/img/Group37.png" alt="">
+            </div>
+        </div>
+        <div class="w-4/6 h-0 border-solid border border-amber-400  flex justify-center items-center">
+        </div>
+        <div class="flex justify-center gap-6 w-3/4">
+            <div class="flex flex-col w-1/3 gap-6">
+                <div class="h-1/3 md:text-4xl text-center flex flex-col items-center justify-center">IT'S TIME TO PLAY
+                    <span class="text-amber-600">SOCCER!</span>
+                </div>
+                <img class="h-2/3" src="../assets/img/Slide2.jpg" alt="">
+            </div>
+            <div class="flex flex-col w-1/4 gap-6   ">
+                <img class="h-1/2" src="../assets/img/slider1.jpg" alt="">
+                <img class="h-1/2" src="../assets/img/slider1.jpg" alt="">
+            </div>
+            <div class="flex flex-col w-1/4 gap-6">
+                <img src="../assets/img/slider1.jpg" alt="">
+                <p class="text-center">Location de terrain, abonnement, <br> championnat, Club House, <br> événement
+                    d'entreprise <br>... Quel que soit votre profil, <br> notre équipe vous accueille toute <br>
+                    l'annéeLocation de terrain, abonnement,
+                </p>
+            </div>
+        </div>
+        <div class="w-4/6 h-0 border-solid border border-amber-400  flex justify-center items-center">
+        </div>
+        <div>
+            <h1 class="text-center text-4xl">hi evry one hh inak inak </h1>
+            <div class="flex w-full  justify-around items-center">
+                <div class="w-1/4">
+                    <img class="w-full" src="../assets/img/Skateboarding.gif" alt="">
+                </div>
+                <div class="w-1/4">
+                    <img class="w-full" src="../assets/img/Soccer.gif" alt="">
+                </div>
+                <div class="w-1/4">
+                    <img class="w-full" src="../assets/img/Basketball.gif" alt="">
+                </div>
+
+            </div>
+        </div>
+        <div class="w-4/6 h-0 border-solid border border-amber-400  flex justify-center items-center">
+        </div>
+        <div class="w-2/6 bg-black h-9">
+        </div>
+        <div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home-vieu',
+}
+</script>
+
+<style lang="scss" scoped>
+</style>

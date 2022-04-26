029bf7e0aa5acd7750a2caf6bfebcee18bf69abc
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-26 22:30:11 +0000

    lien in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router/index";
 import './index.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+AOS.init();
+document.addEventListener('aos:in', ({ detail }) => {
+console.dir(detail);
+});
 
 createApp(App).use(router).mount('#app')

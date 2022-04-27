f3d49bdeded5c8ae9b0388bc296d965fdb25465a
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-04-27 14:47:22 +0000

    Login

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,59 +1,70 @@
 <template>
-<div class="flex flex-col justify-center items-center gap-20">
-    <div class="flex flex-col items-center justify-center w-full mt-20 gap-5">
-        <h1 class="font-bold md:text-5xl sm:text-xl text-center">A small business is only as good as <br> its tools.
-        </h1>
-        <p class="text-center sm:text-lg ">We’re different. Flex is the only saas business platform that lets you
-            run your business on one platform, seamlessly across all digital channels.</p>
-        <div stye='height:900px;' class="w-1/2 h-[600px] relative">
-            <img class="w-full h-full" src="../assets/img/Group37.png" alt="">
-        </div>
-    </div>
-    <div class="w-4/6 h-0 border-solid border border-amber-400  flex justify-center items-center">
-    </div>
-    <div class="flex justify-center gap-6 w-3/4">
-        <div class="flex flex-col w-1/3 gap-6">
-            <div class="h-1/3 md:text-4xl text-center flex flex-col items-center justify-center">IT'S TIME TO PLAY
-                <span class="text-amber-600">SOCCER!</span>
-            </div>
-            <img class="" data-aos="zoom-in-down" src="../assets/img/Slide2.jpg" alt="">
-        </div>
-        <div class="flex flex-col w-1/4 gap-6   ">
-            <img data-aos="zoom-in-down" data-aos-once="false" class="" src="../assets/img/slider1.jpg" alt="">
-            <img data-aos="zoom-in-down" data-aos-once="false" class="" src="../assets/img/slider1.jpg" alt="">
-        </div>
-        <div class="flex flex-col w-1/4 gap-6">
-            <img data-aos="zoom-in-down" data-aos-once="false" src="../assets/img/slider1.jpg" alt="">
-            <p class="text-center">Location de terrain, abonnement, <br> championnat, Club House, <br> événement
-                d'entreprise <br>...
-            </p>
-        </div>
-    </div>
-    <div class="w-4/6 h-0 border-solid border border-amber-400  flex justify-center items-center">
-    </div>
-    <div>
-        <h1 class="text-center text-4xl">hi evry one hh inak inak </h1>
-        <div class="flex w-full  justify-around items-center">
-            <div class="w-1/4">
-                <img data-aos="fade-up" data-aos-duration="1500" class="w-full" src="../assets/img/Skateboarding.gif" alt="">
-            </div>
-            <div class="w-1/4">
-                <img data-aos="fade-up" data-aos-duration="1500" class="w-full" src="../assets/img/Soccer.gif">
-            </div>
-            <div class="w-1/4">
-                <img data-aos="fade-up" data-aos-duration="1500" class="w-full" src="../assets/img/Basketball.gif" alt="">
+    <div class="flex flex-col justify-center items-center gap-20">
+        <div class="flex flex-col items-center justify-center w-full mt-20 gap-5">
+            <h1 class="font-bold md:text-5xl sm:text-xl text-center">A small business is only as good as <br> its tools.
+            </h1>
+            <p class="text-center sm:text-lg ">Welcome to the Sport Time website. Now is the time to book a time to play
+                in the nearest stadium.</p>
+            <div stye='height:900px;' class="w-1/2 h-[600px] relative">
+                <img class="w-full h-full" src="../assets/img/Group37.png" alt="">
             </div>
         </div>
+        <div class="w-4/6 h-0 border-solid border border-amber-400  flex justify-center items-center">
+        </div>
+        <div class="flex justify-center gap-6 w-3/4">
+            <div class="flex flex-col w-1/3 gap-6">
+                <div class="h-1/3 md:text-4xl text-center flex flex-col items-center justify-center">IT'S TIME TO PLAY
+                    <span class="text-amber-600">SOCCER!</span>
+                </div>
+                <img class="" data-aos="zoom-in-down" data-aos-duration="2000" data-aos-once="true"
+                    src="../assets/img/Slide2.jpg" alt="">
+            </div>
+            <div class="flex flex-col w-1/4 gap-6   ">
+                <img data-aos="zoom-in-down" data-aos-once="true" data-aos-duration="2000" class=""
+                    src="../assets/img/slider1.jpg" alt="">
+                <img data-aos="zoom-in-down" data-aos-once="true" data-aos-duration="2000" class=""
+                    src="../assets/img/slider1.jpg" alt="">
+            </div>
+            <div class="flex flex-col w-1/4 gap-6">
+                <img data-aos="zoom-in-down" data-aos-once="true" data-aos-duration="2000"
+                    src="../assets/img/slider1.jpg" alt="">
+                <p class="text-center">Location de terrain, abonnement, <br> championnat, Club House, <br> événement
+                    d'entreprise <br>...
+                </p>
+            </div>
+        </div>
+        <div class="w-4/6 h-0 border-solid border border-amber-400  flex justify-center items-center">
+        </div>
+        <div>
+            <h1 class="text-center text-4xl">All kinds of sports are available in your nearest stadium</h1>
+            <div class="flex w-full  justify-around items-center">
+                <div class="w-1/4">
+                    <img data-aos="fade-up" data-aos-once="true" data-aos-delay="500" data-aos-duration="3000"
+                        class="w-full" src="../assets/img/Skateboarding.gif" alt="">
+                </div>
+                <div class="w-1/4">
+                    <img data-aos="fade-up" data-aos-once="true" data-aos-delay="500" data-aos-duration="3000"
+                        class="w-full" src="../assets/img/Soccer.gif">
+                </div>
+                <div class="w-1/4">
+                    <img data-aos="fade-up" data-aos-once="true" data-aos-delay="500" data-aos-duration="3000"
+                        class="w-full" src="../assets/img/Basketball.gif" alt="">
+                </div>
+            </div>
+        </div>
+        <div v-if="Stars" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="2000" data-aos-delay="3000"
+            id="feedback"
+            class="flex flex-col gap-4 justify-center items-center bg-green-400  fixed right-4 bottom-5 bg-base-100 shadow-xl ">
+            <Feedback class="relative" />
+            <Icon @click="Stars = false" class="absolute top-1 right-2 cursor-pointer text  text-3xl z-10 "
+                color="white" icon="bi:x-lg" />
+            <textarea class="rounded p-3 border" name="" id="textAria" rows="3" cols="30"
+                placeholder="send the feedback"></textarea>
+            <button class="text-white w-full py-2 rounded bg-orange-400 ">Send</button>
+        </div>
+        <div>
+        </div>
     </div>
-    <div v-if="Stars" id="feedback" class="flex flex-col gap-4 justify-center items-center bg-green-400  fixed right-4 bottom-5 bg-base-100 shadow-xl ">
-        <Feedback class="relative" />
-        <Icon @click="Stars = false" class="absolute top-1 right-2 cursor-pointer text  text-3xl z-10 " color="white" icon="bi:x-lg" />
-        <textarea class="rounded p-3 border" name="" id="textAria" rows="3" cols="30" placeholder="send the feedback"></textarea>
-        <button class="text-white w-full py-2 rounded bg-orange-400 ">Send</button>
-    </div>
-    <div>
-    </div>
-</div>
 </template>
 
 <script>
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,19 +1,86 @@
 <template>
-<div>
-    <div>
-        <h1>h1hihigf hfidohigohsdfihgiofhdo</h1>
-        <h2>h3ifdohigohdfiohgiohfdioghio</h2>
+    <div class="flex justify-center items-center w-full ">
+        <div class="flex justify-center items-center w-full ">
+            <div class="flex justify-center items-center gap-20  w-full form_Login">
+                <div data-aos="flip-left" data-aos-easing="linear" data-aos-duration="1500" data-aos-delay="1000"
+                    class=" relative h-full flex flex-col gap-3">
+                    <div class="flex flex-col justify-center items-center ">
+                        <h1 class="form_Login_title">Sig Up in a new account</h1>
+                    </div>
+                    <form class="form_Login_formData" action="">
+                        <label for="email">Entrer your email</label>
+                        <input type="email" placeholder="A.STAILI@student.youcode.com" name="email">
+                        <label for="password">password</label>
+                        <input type="password" placeholder="**************" name="password">
+                        <input id="submit" type="submit" name="valid" value="Sing Up">
+                    </form>
+                </div>
+                <div data-aos="fade-left" data-aos-anchor="#example-anchor" data-aos-delay="500" data-aos-offset="500">
+                    <img src="../assets/img/login.gif" alt="">
+                </div>
+            </div>
+        </div>
     </div>
-    <form action="">
-        <input type="gmail">
-        <input type="password">
-        <input type="submit">
-    </form>
-</div>
 </template>
 
 <script>
 export default {
-    name:"sin-gin"
+    name: "sin-gup",
+    methods: {
+
+    },
 }
 </script>
+
+<style>
+.form_Login {
+    margin: 110px;
+
+    border-radius: 16px;
+}
+
+.form_Login_title {
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: rgba(255, 159, 28, 1);
+}
+
+
+.form_Login_formData {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: flex-start;
+    background-color: rgb(255, 255, 255);
+
+}
+
+.form_Login_formData label {
+    text-align: start;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+
+}
+
+.form_Login_formData input {
+    border: 1px solid #D5DAE1;
+    box-sizing: border-box;
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 8px;
+    width: 500px;
+    height: 2.5rem;
+    outline: none;
+    padding: 10px;
+    margin-bottom: 1rem;
+}
+
+#submit {
+    height: 3rem;
+    margin-top: 10px;
+    background-color: rgba(34, 197, 94, 1);
+    color: #fff;
+}
+</style>

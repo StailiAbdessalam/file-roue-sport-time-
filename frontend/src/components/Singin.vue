045350844f705375fb13c645dd33ef2437f3142d
benjy33x <abdoycode@gmail.com>
2022-04-27 00:27:31 +0000

    sing

---
+++ frontend/src/components/Singin.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <div>
+        <img class="w-36" src="../assets/img/LOGO.png" alt="">
+        <h1>h1</h1>
+        <h2>h3</h2>
+    </div>
+    <form action="">
+        <input type="gmail">
+        <input type="password">
+        <input type="submit">
+        <a href=""></a>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+    name:"sin-gin"
+}
+</script>

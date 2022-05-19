132c67b147f34285716e112441d31a12ff8cfacc
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-05-19 11:37:30 +0100

    read all reservation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,6 @@
     <NavBar />
     <NavUser v-if="Role == 'Client'" />
     <NavOrganisateur v-if="Role == 'organisateur'" />
-    <profile />
     <router-view />
     <Footer v-if='Role!="Admin"' />
 
@@ -18,12 +17,11 @@ import NavAdmin from './components/Admin/NavAdmin.vue';
 import NavUser from './components/Users/NavUser.vue';
 import NavBar from './components/NavBar.vue';
 import Footer from './components/Footer.vue';
-import profile from './components/Admin/Profile'
 import {computed} from "vue";
 
 export default {
   name: 'component-vue',
-  components: { NavAdmin, NavBar, Footer, profile, NavUser, NavOrganisateur },
+  components: { NavAdmin, NavBar, Footer, NavUser, NavOrganisateur },
   provide(){
     return {
       setRole: this.setRole,

d6d8180d8faf84d78fb20b2c9a586f53463d8ff7
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-05-20 10:39:41 +0100

    add controller stade

---
+++ BackEnd/app/controllers/Stade.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:*');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+}
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") return true;
+class Stade extends Controller
+{
+    public $valide = "false";
+    public function __construct()
+    {
+    }
+
+    public function AddStade()
+    {
+      $CliensModel = $this->model('StadeModel');
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json);
+        $data = (array)$data;
+        $CliensModel->insert($data);
+        echo json_encode("true");
+      }
+    }
+}

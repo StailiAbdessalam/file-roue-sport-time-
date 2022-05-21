c866a3ddff45604d1e7e260c956033ce71d5bb8c
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-05-21 12:04:56 +0100

    read stade avec id local

---
+++ frontend/src/Pages/Organisateur/AllStade.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <AllStade />
+  </div>
+</template>
+
+<script>
+import AllStade from "../../components/Organisateur/AllStade.vue";
+export default {
+  name: "All-Stade",
+  components: {
+    AllStade,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

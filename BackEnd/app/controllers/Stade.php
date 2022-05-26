7f40cca05aba57d8660092aec6b233fe44e7442a
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-05-26 18:18:59 +0100

    stade controller getAllTime

--- BackEnd/app/controllers/Stade.php
+++ BackEnd/app/controllers/Stade.php
@@ -37,4 +37,14 @@ class Stade extends Controller
       echo json_encode($local);
     }
   }
+  public function getAllTime(){
+    $Model= $this->model('StadeModel');
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $json = file_get_contents('php://input');
+      $data = json_decode($json);
+
+      $local = $Model->getAllTime($data);
+      echo json_encode($local);
+    }
+  }
 }

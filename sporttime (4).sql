605bdae6d429d56805d6273cdeb06243310d6efe
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-05-28 13:27:04 +0100

    fille sql

--- sporttime (4).sql
+++ sporttime (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 22 mai 2022 à 17:48
+-- Généré le : ven. 27 mai 2022 à 20:32
 -- Version du serveur : 10.4.22-MariaDB
 -- Version de PHP : 8.1.2
 
@@ -113,7 +113,10 @@ INSERT INTO `clients` (`id`, `FirstName`, `LastName`, `UserName`, `Phone`, `IdUn
 (57, 'Sarah', 'Wiley', 'dolipiv', '+1 (496) 655-5567', 'CL-62861ef84f955', 'dilubulu@mailinator.com'),
 (58, 'Ivory', 'Savage', 'zuzola', '+1 (918) 526-5234', 'CL-62867285b9673', 'buli@mailinator.com'),
 (59, 'Darius', 'Newton', 'nanaweviw', '+1 (885) 725-9089', 'CL-6287b51fee71a', 'dyjyn@mailinator.com'),
-(60, 'Graiden', 'Gay', 'rawewo', '+1 (754) 811-8549', 'CL-6287d7eb1648e', 'sebenunele@mailinator.com');
+(60, 'Graiden', 'Gay', 'rawewo', '+1 (754) 811-8549', 'CL-6287d7eb1648e', 'sebenunele@mailinator.com'),
+(61, 'Chava', 'Morgan', 'tibone', '+1 (887) 695-3953', 'CL-628a676964b33', 'gixipecix@mailinator.com'),
+(62, 'Justina', 'Juarez', 'byxomaduz', '+1 (212) 725-9067', 'CL-628d148b507e7', 'romuce@mailinator.com'),
+(63, 'Medge', 'Mejia', 'zohycekur', '+1 (737) 867-9977', 'CL-6290e5476d431', 'nige@mailinator.com');
 
 -- --------------------------------------------------------
 
@@ -139,7 +142,9 @@ INSERT INTO `local` (`id`, `idOrganisateur`, `Address`, `Ville`, `Photo`, `About
 (35, 63, 'local 2', 'local 2', '3z4x1qywdcrl3erexvr', 'local 2 local 2 local 2 local 2local 2 local 2local 2 local 2local 2 local 2local 2 local 2local 2 local 2'),
 (36, 64, 'Totam repellendus N', 'Molestiae tempor mol', 'dvbk73q10z9l3ev4yyl', 'hedufhruph fur giurhugho ragohazoghio zegzoighoiezh ogrgezhgoezhigo hzegdsu vohmezihehgepmouez hgp'),
 (37, 65, 'Architecto qui minim', 'In ullam blanditiis ', 'v7yrpjcidufl3ev5qvi', 'Lorem eaque exercitarefugzreuifguezigfuigeziufgezf gezfgeuizgfuiezgiufgezuigfuiegfpugzeui'),
-(38, 66, 'Maiores perferendis ', 'Debitis dolor obcaec', '01y1xwdzziill3ev6u5j', 'Praesentium qui irur hlfqgomfgapzf eazm fmef abdessalm  syatdoil  hdsmhf dsfdmhsfjhds');
+(38, 66, 'Maiores perferendis ', 'Debitis dolor obcaec', '01y1xwdzziill3ev6u5j', 'Praesentium qui irur hlfqgomfgapzf eazm fmef abdessalm  syatdoil  hdsmhf dsfdmhsfjhds'),
+(39, 68, 'jkfhjksh', 'meknes', 'hvb1jdxcpyll3ki1wz4', 'the best local stadieum'),
+(40, 90, 'LBYAR terrain lbyar ', 'SAFI', 'tf5j29vhijl3ol7t3w', 'haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa chri ou bi3 ou reserver terrain m3a hakimi ');
 
 -- --------------------------------------------------------
 
@@ -163,14 +168,36 @@ CREATE TABLE `organisateur` (
 --
 
 INSERT INTO `organisateur` (`id`, `FirstName`, `LastName`, `business_name`, `Email`, `Phone`, `IdUnique`, `suspended`) VALUES
-(57, '4anjo', ' STAILI', 'BENJY33X', 'A.STAILI@student.youcode.ma', '0646906318', 'OR-62867d3756772', '1'),
+(57, '4anjo', ' STAILI', 'BENJY', 'A.STAILI@student.youcode.ma', '0646906318', 'OR-62867d3756772', '1'),
 (58, 'Victoria Lowe', 'Mccray', 'Scarlet Petersen', 'miliqoti@mailinator.com', '+1 (845) 387-5017', 'OR-62868b7332101', '0'),
 (59, 'Octavius Oneill', 'Salazar', 'Lionel Carver', 'qepypape@mailinator.com', '+1 (868) 747-7918', 'OR-6287d76cee8ed', '0'),
 (62, 'Natalie Puckett', 'Mccoy', 'Wallace Foley', 'nobiwixog@mailinator.com', '+1 (881) 998-3208', 'OR-6287d7f550685', '0'),
 (63, 'test 2', 'test 2', 'test 2', 'sebenunele@mailinator.com', '12345678', 'OR-6287d8205cc79', '1'),
-(64, 'Erich Gibbs', 'Ramirez', 'Roary Barlow', 'zazisol@mailinator.com', '+1 (477) 401-1689', 'OR-6287f26817648', '1'),
+(64, 'Erich Gibbs', 'Ramirez', 'Roary Barlow', 'zazisol@mailinator.com', '+1 (477) 401-1689', 'OR-6287f26817648', 'X'),
 (65, 'Melissa Morse', 'Farley', 'Herrod Bridges', 'gekafymo@mailinator.com', '+1 (678) 784-7668', 'OR-6287f294b0b2a', '1'),
-(66, 'Jerry Hicks', 'Booth', 'Igor Strong', 'lahocaqive@mailinator.com', '+1 (494) 185-3649', 'OR-6287f2c027984', '1');
+(66, 'Jerry Hicks', 'Booth', 'Igor Strong', 'lahocaqive@mailinator.com', '+1 (494) 185-3649', 'OR-6287f2c027984', '1'),
+(67, 'Teegan Soto', 'Abbott', 'Kato Wilder', 'jokij@mailinator.com', '+1 (653) 379-7697', 'OR-628cf5656cd03', '0'),
+(68, 'George Dunn', 'Meyers', 'Driscoll Moss', 'qygohixod@mailinator.com', '+1 (445) 619-2928', 'OR-628d258ecf53c', '1'),
+(70, 'Ebony Hess', 'Golden', 'Knox Obrien', 'huzes@mailinator.com', '+1 (657) 552-1071', 'OR-628e658235681', '0'),
+(71, 'ergg', 'regre', 'regreg', 'EZR@gmail.com', '43223123', 'OR-628e659c3edb8', '0'),
+(73, 'TAH', 'LCHGARE', 'TAHA_LACHGARE', 'A.CHGARE@gmaiddl.com', '23456789', 'OR-6290e3895cafc', '0'),
+(74, 'Farrah Browning', 'Long', 'Jeremy Koch', 'mehove@mailinator.com', '+1 (244) 758-2991', 'OR-6290e3b7bbeee', '0'),
+(75, 'Palmer Bean', 'Berg', 'Bruce Bond', 'tucyzova@mailinator.com', '+1 (911) 113-4811', 'OR-6290e45c6da54', '0'),
+(76, 'Jillian Bruce', 'Acosta', 'Basil Curtis', 'diwogasuro@mailinator.com', '+1 (942) 918-8065', 'OR-6290e4b255689', '0'),
+(77, 'Daryl Barry', 'Perry', 'Idola Pickett', 'pecyt@mailinator.com', '+1 (855) 313-2154', 'OR-6290e4d133466', '0'),
+(78, 'Kenneth Dunn', 'Burton', 'Kaitlin Daniel', 'qugimef@mailinator.com', '+1 (839) 902-2872', 'OR-6290e4f14e433', '0'),
+(79, 'Conan Tran', 'Warner', 'Tate Acosta', 'qososumowe@mailinator.com', '+1 (507) 627-8487', 'OR-6290e5b3a9194', '0'),
+(80, 'Orla Kirkland', 'Harmon', 'Lester Nguyen', 'remofixe@mailinator.com', '+1 (432) 225-7996', 'OR-6290e5df05beb', '0'),
+(81, 'Caryn Richardson', 'Jarvis', 'Brent Beard', 'pezyruw@mailinator.com', '+1 (321) 918-1038', 'OR-6290e74b71854', '0'),
+(82, 'Nina Griffith', 'Bridges', 'Wylie William', 'ragycekopy@mailinator.com', '+1 (862) 301-5117', 'OR-6290e77d5c2d3', '0'),
+(83, 'Kirby Mcleod', 'Turner', 'Jason Roberts', 'sujini@mailinator.com', '+1 (952) 819-3695', 'OR-6290e79d61e83', '0'),
+(84, 'Daria Morrow', 'Davidson', 'Travis Albert', 'daqus@mailinator.com', '+1 (388) 911-7134', 'OR-6290e7e78dadc', '0'),
+(85, 'Jarrod Pittman', 'Savage', 'Elizabeth Fitzgerald', 'rynipeh@mailinator.com', '+1 (401) 406-5161', 'OR-6290e8c2217ca', '0'),
+(86, 'Simon Hensley', 'Ryan', 'Craig Spears', 'fupuxode@mailinator.com', '+1 (807) 814-3924', 'OR-6290e8ebb5d31', '0'),
+(87, 'Kasimir Santos', 'Price', 'Chanda Park', 'xiberipyxo@mailinator.com', '+1 (712) 338-1553', 'OR-6290e90ea9ea5', '0'),
+(88, 'Wilma Huff', 'Weeks', 'Olympia Carpenter', 'lijifejo@mailinator.com', '+1 (281) 634-4928', 'OR-6290e922380e9', '0'),
+(89, 'Fleur Mccullough', 'Mendoza', 'Bruno Dale', 'qukybu@mailinator.com', '+1 (151) 579-3621', 'OR-6290e963aad73', '0'),
+(90, 'ABdo²', 'Terrain', 'Cadman Mayo', 'fiweduqy@mailinator.com', '+1 (664) 291-4469', 'OR-6290e9850fa83', '1');
 
 -- --------------------------------------------------------
 
@@ -181,10 +208,36 @@ INSERT INTO `organisateur` (`id`, `FirstName`, `LastName`, `business_name`, `Ema
 CREATE TABLE `reservation` (
   `id` int(11) NOT NULL,
   `idStadieum` int(11) NOT NULL,
-  `idOrganisateur` int(11) NOT NULL,
+  `idLocal` int(11) NOT NULL,
   `idClient` int(11) NOT NULL,
-  `Time` datetime NOT NULL
+  `Time` varchar(255) NOT NULL,
+  `Date` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `idStadieum`, `idLocal`, `idClient`, `Time`, `Date`) VALUES
+(17, 10, 34, 1, 'def', 'ede'),
+(20, 10, 34, 1, '4 PM', '2022-05-03'),
+(21, 12, 34, 1, '15 PM', '2022-05-27'),
+(23, 10, 34, 1, '14 PM', '2022-05-12'),
+(24, 12, 39, 31, '12 PM', '2022-05-03'),
+(25, 10, 34, 48, '12', '2022-05-03'),
+(26, 10, 34, 1, '10 PM', '2022-05-03'),
+(27, 12, 34, 1, '14 PM', '2022-05-26'),
+(28, 10, 34, 1, '11 PM', '2022-05-03'),
+(29, 10, 34, 1, '16 PM', '2022-05-03'),
+(30, 10, 34, 1, '22 PM', '2022-05-03'),
+(31, 10, 34, 1, '15 PM', '2022-05-30'),
+(32, 10, 34, 1, '9 AM', '2022-05-02'),
+(33, 10, 34, 1, '8 AM', '2022-05-03'),
+(34, 10, 34, 1, '5 AM', '2022-05-04'),
+(35, 10, 34, 1, '9 AM', '2022-05-04'),
+(36, 10, 34, 1, '6 AM', '2022-05-03'),
+(37, 10, 34, 1, '10 AM', '2022-05-03'),
+(38, 10, 34, 1, '7 AM', '2022-05-03');
 
 -- --------------------------------------------------------
 
@@ -208,13 +261,15 @@ CREATE TABLE `stadium` (
 --
 
 INSERT INTO `stadium` (`id`, `idLocal`, `Title`, `Description`, `Type`, `num_personne`, `images`, `priceH`) VALUES
-(3, 34, 'Voluptate culpa susc', 'Labore dolor elit r', 'auther', 39, 'qdrcbg1bbcnl3e8tyv8', 79),
-(4, 34, 'Ut dolorem non autem', 'Doloremque commodo a', 'auther', 27, '2gy7v2t0zjgl3eivcbd', 52),
-(5, 34, 'Fugit ullam cum aut', 'Exercitationem excep', 'Football', 29, 'w0tzzp92d7dl3eixkyj', 52),
-(6, 34, 'Porro molestiae sint', 'Vel aliquip sit comm', 'basketball', 70, 'mj7upixketl3eizf2b', 56),
-(7, 34, 'Et consequatur quas', 'Exercitationem lauda', 'basketball', 60, 'ilm71nub6zl3ej07vs', 57),
-(8, 34, 'Sed ut cillum sed no', 'A suscipit sint ut ', 'auther', 78, 'pk2mozmm7yol3ej1y24', 54),
-(9, 34, 'Local 2 ', 'Local 2 Local 2 Local 2 Local 2 Local 2 Local 2 Local 2 Local 2 Local 2 Local 2 Local 2 Local 2 ', 'basketball', 2, 'ns3i0x387sel3eqzg69', 12);
+(10, 34, 'stade', 'the bon stade', 'basketball', 12, 'm4y59ut3kyl3hhgepr', 100),
+(12, 34, 'bug', 'bug', 'basketball', 12, 's1n5h26kk4l3lvmr1m', 121),
+(13, 40, 'lbyare asta ', 'zegfom ezgf uemgfuzeg fezg mofgo', 'basketball', 6, 'ympc7je1rosl3olglqk', 50),
+(14, 40, 'Nisi nihil corrupti', 'Qui odio inventore q', 'auther', 88, '9puiqo9gnwtl3olp0uv', 16),
+(15, 40, 'Officia alias sunt c', 'Ea aut cum officia a', 'Football', 70, 'rhymvu1w7cbl3om3vwv', 81),
+(16, 40, 'Fugiat officia aliq', 'Quidem facere quos a', 'auther', 54, 'kdw27lnavkl3om4kor', 79),
+(17, 40, 'Adipisicing ducimus', 'Corrupti accusamus ', 'basketball', 20, 'cbmwoinnkjl3om5ugb', 93),
+(18, 40, 'Dolorum accusantium ', 'Lorem officiis et mo', 'auther', 97, 'd3qwnnhk6rjl3om94vy', 13),
+(19, 40, 'Quidem voluptates hi', 'Aliquip consequuntur', 'auther', 63, 'bn88lt7ps4l3oma2hi', 50);
 
 --
 -- Index pour les tables déchargées
@@ -259,11 +314,7 @@ ALTER TABLE `organisateur`
 -- Index pour la table `reservation`
 --
 ALTER TABLE `reservation`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `Time` (`Time`),
-  ADD KEY `idClient` (`idClient`),
-  ADD KEY `idStadieum` (`idStadieum`),
-  ADD KEY `idOrganisateur` (`idOrganisateur`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `stadium`
@@ -286,31 +337,31 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT pour la table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT pour la table `local`
 --
 ALTER TABLE `local`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT pour la table `organisateur`
 --
 ALTER TABLE `organisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
 
 --
 -- AUTO_INCREMENT pour la table `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT pour la table `stadium`
 --
 ALTER TABLE `stadium`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Contraintes pour les tables déchargées
@@ -328,7 +379,7 @@ ALTER TABLE `local`
 ALTER TABLE `reservation`
   ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`idClient`) REFERENCES `clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`idStadieum`) REFERENCES `stadium` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`idOrganisateur`) REFERENCES `organisateur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`idLocal`) REFERENCES `local` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `stadium`

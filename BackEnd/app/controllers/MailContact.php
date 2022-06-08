d0a36100f0e8cf5493e86d39744f943e06e63258
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-06-08 18:32:07 +0100

    Mail conatact

--- BackEnd/app/controllers/MailContact.php
+++ BackEnd/app/controllers/MailContact.php
@@ -1,13 +1,14 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Methods:*');
+header('Access-Control-Allow-Methods:POST');
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
   if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
   if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 }
 if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") return true;
-class SendMAIL extends Controller
+
+class MailContact extends Controller
 {
     public $valide = "false";
     public function __construct()
@@ -15,12 +16,21 @@ class SendMAIL extends Controller
     }
     public function sendEmail()
     {
-        $CreateAcc = $this->model('SendEmail');
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $json = file_get_contents('php://input');
             $data = json_decode($json);
-            var_dump($data);
-            $CreateAcc->admin($data);
+            $to  = 'abdoycode@gmail.com';
+            $subject = 'Contact Client';
+            $headers[] = 'MIME-Version: 1.0';
+            $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+            $headers[] = "To: Sport Time <abdoycode@gmail.com>";
+            $headers[] = "From: $data->email";
+            mail($to, $subject, $data->sujet, implode("\r\n", $headers));
+            var_dump(mail($to, $subject, $data->sujet, implode("\r\n", $headers)));
+            if(mail){
+                echo "goo";
+            }
+          
         }
     }
 }

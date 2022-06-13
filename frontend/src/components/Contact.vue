ef5d798270e22c798bf81449e594f66185e1fad2
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-06-13 02:30:58 +0100

    components contact

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -1,105 +1,66 @@
 <template>
-  <section
-    class="py-20 px-4 lg:px-16 overflow-hidden relative z-10 flex justify-center items-center w-full"
-    data-aos="fade-up"
-    id="contact"
-  >
-    <div class="container flex-col justify-center items-center">
-      <div class="mb-5 flex items-center max-w-md">
-        <h2 class="text-slate-900 text-yellow-400 text-3xl font-bold">
-          Contact Me
-        </h2>
-      </div>
-      <div
-        class="flex flex-col lg:flex-row lg:items-center text-slate-900 lg:justify-between -mx-4"
-      >
-        <div
-          class="w-full lg:w-1/2 xl:w-6/12 px-4"
-          data-aos="fade-up"
-          data-aos-delay="200"
-        >
-          <div class="mb-12 lg:mb-0">
-            <p
-              class="text-base md:text-2xl sm:text-sm font-extrabold text-body-color leading-relaxed stand__out__text mb-9 mx-5"
-            >
-              Lorem ipsum dolor. Nulla porttitor accumsan tincidunt. Lorem ipsum
-              dolor. Nulla porttitor accumsan tincidunt. Lorem ipsum dolor.
-              Nulla porttitor accumsan tincidunt.
-            </p>
-          </div>
-        </div>
-        <div
-          class="w-full lg:w-1/2 xl:w-5/12 px-4"
-          data-aos="fade-up"
-          data-aos-delay="500"
-          data-aos-duration="2000"
-        >
-          <div class="bg-gray-100 relative rounded-lg p-8 sm:p-12 shadow-lg">
-            <form @submit.prevent>
-              <div class="mb-6">
-                <input
-                  type="text"
-                  placeholder="Your Name"
-                  v-model="form.name"
-                  class="w-full rounded p-3 text-gray-800 border-gray-500 outline-none focus-visible:shadow-none focus:border-primary"
-                  name="full_name"
-                  id="full_name"
-                />
-              </div>
-              <div class="mb-6">
-                <input
-                  type="email"
-                  v-model="form.email"
-                  placeholder="Your Email"
-                  class="w-full rounded p-3 text-gray-800 border-gray-500 outline-none focus-visible:shadow-none focus:border-primary"
-                  name="email"
-                  id="email"
-                />
-              </div>
-
-              <div class="mb-6">
-                <textarea
-                  rows="6"
-                  v-model="form.sujet"
-                  placeholder="Your Message"
-                  class="w-full rounded p-3 text-gray-800 border-gray-500 outline-none focus-visible:shadow-none focus:border-primary"
-                  name="message"
-                  id="message"
-                ></textarea>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  @click="sendMessage"
-                  class="w-full text-gray-100 hover:text-gray-700 bg-yellow-400 rounded border border-primary p-3 transition ease-in-out duration-500 hover:bg-yellow-300"
-                >
-                  Send Message
-                </button>
-              </div>
-            </form>
-            <div>
-              <span class="absolute -top-10 -right-9 z-[-1]">
-                <svg
-                  width="100"
-                  height="100"
-                  viewBox="0 0 100 100"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M0 100C0 44.7715 0 0 0 0C55.2285 0 100 44.7715 100 100C100 100 100 100 0 100Z"
-                    fill="#FACC15"
-                  />
-                </svg>
-              </span>
+<!-- components Contact -->
+  <div class="w-full">
+    <div class="bg-gradient-to-b from-green-800 to-green-400 h-96"></div>
+    <div class="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
+      <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
+        <p class="text-3xl font-bold leading-7 text-center">Contact me</p>
+        <form @submit.prevent>
+          <div class="md:flex items-center mt-12">
+            <div class="w-full md:w-1/2 flex flex-col">
+              <label class="font-semibold leading-none">Name</label>
+              <input
+                placeholder="Your Name"
+                v-model="form.name"
+                type="text"
+                class="leading-none text-gray-900 p-3 focus:outline-none focus:border-green-700 mt-4 bg-gray-100 border rounded border-gray-200"
+              />
+            </div>
+            <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
+              <label class="font-semibold leading-none">Phone</label>
+              <input
+                placeholder="Your Phone"
+                type="number"
+                class="leading-none text-gray-900 p-3 focus:outline-none focus:border-green-700 mt-4 bg-gray-100 border rounded border-gray-200"
+              />
             </div>
           </div>
-        </div>
+          <div class="md:flex items-center mt-8">
+            <div class="w-full flex flex-col">
+              <label class="font-semibold leading-none">Email</label>
+              <input
+                v-model="form.email"
+                placeholder="Your Email"
+                type="Email"
+                class="leading-none text-gray-900 p-3 focus:outline-none focus:border-green-700 mt-4 bg-gray-100 border rounded border-gray-200"
+              />
+            </div>
+          </div>
+          <div>
+            <div class="w-full flex flex-col mt-8">
+              <label class="font-semibold leading-none">Message</label>
+              <textarea
+                v-model="form.sujet"
+                placeholder="Your Message"
+                type="text"
+                class="h-40 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-green-700 mt-4 bg-gray-100 border rounded border-gray-200"
+              ></textarea>
+            </div>
+          </div>
+          <div
+            @click="sendMessage"
+            class="flex items-center justify-center w-full"
+          >
+            <button
+              class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-green-700 rounded hover:bg-green-600 focus:ring-2 focus:ring-offset-2 focus:ring-green-700 focus:outline-none"
+            >
+              Send message
+            </button>
+          </div>
+        </form>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -120,7 +81,7 @@ export default {
     sendMessage() {
       console.log(this.form);
       axios
-        .post(`${this.$apiUrl}/MailContact/sendEmail`,{
+        .post(`${this.$apiUrl}/MailContact/sendEmail`, {
           name: this.form.name,
           email: this.form.email,
           sujet: this.form.sujet,
@@ -138,6 +99,13 @@ export default {
             sujet: "",
           };
         });
+    },
+    togglemessage(index, ray) {
+      if (ray == 1) {
+        this.questio1[index].show = !this.questio1[index].show;
+      } else {
+        this.questio2[index].show = !this.questio2[index].show;
+      }
     },
   },
 };

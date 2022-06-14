735b87cb89972f69e1d8a84a1fb740a23e94ef2e
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-06-14 16:27:41 +0100

    Myreservation

---
+++ frontend/src/Pages/Users/MyReservation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Reservation />
+  </div>
+</template>
+
+<script>
+import Reservation from "../../components/Users/MyReservation.vue";
+export default {
+  name: "myrese-rvations",
+  components: {
+    Reservation,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

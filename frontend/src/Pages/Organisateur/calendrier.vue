68ebeaa32a863077c5bb7250b831b64641d5f861
BENJY33X <92626379+BENJY33X@users.noreply.github.com>
2022-06-17 19:28:20 +0100

    addd calendrier components

---
+++ frontend/src/Pages/Organisateur/calendrier.vue
@@ -0,0 +1,15 @@
+<template>
+  <Calendar/>
+</template>
+
+<script>
+import Calendar from "../../components/Organisateur/Calendar"
+export default {
+  name: "calen-drier",
+  components: {
+    Calendar,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
